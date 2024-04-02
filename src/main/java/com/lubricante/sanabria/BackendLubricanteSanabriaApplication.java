d36c36206a2f0332fb67130694f90940923a0ae9
Santiago Bocanegra Gómez <70662179+SantiBocanegra@users.noreply.github.com>
2024-04-01 22:38:36 -0500

    Primer commit del proyecto lubricante sanabria

---
+++ src/main/java/com/lubricante/sanabria/BackendLubricanteSanabriaApplication.java
@@ -0,0 +1,13 @@
+package com.lubricante.sanabria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendLubricanteSanabriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendLubricanteSanabriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lubricante/sanabria/BackendLubricanteSanabriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lubricante.sanabria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendLubricanteSanabriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
